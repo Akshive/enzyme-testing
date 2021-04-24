e2a1acfec1017398111bff7749019eb5fb221372
Akshive <pandeyakshive97@gmail.com>
2021-04-24 23:14:12 +0530

    adding shallow tests

---
+++ src/components/__tests__/List.test.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { shallow } from "../../enzyme";
+
+import List from "../List";
+
+describe("List tests", () => {
+  it("renders list-items", () => {
+    const items = ["one", "two", "three"];
+    const wrapper = shallow(<List items={items} />);
+
+    expect(wrapper.find(".list-items")).toBeDefined();
+    expect(wrapper.find(".item")).toHaveLength(items.length);
+  });
+
+  it("reders a list item", () => {
+    const items = ["Thor", "Loki"];
+    const wrapper = shallow(<List items={items} />);
+
+    expect(
+      wrapper.contains(
+        <li key="Thor" className="item">
+          Thor
+        </li>
+      )
+    ).toBeTruthy();
+  });
+
+  it("renders correct text in item", () => {
+    const items = ["John", "James", "Luke"];
+    const wrapper = shallow(<List items={items} />);
+
+    expect(wrapper.find(".item").get(0).props.children).toEqual("John");
+  });
+});
